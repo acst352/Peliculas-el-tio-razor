b7c27864fd49003d79c25b5e83a838c5c6b4047d
Alex <acst352@gmail.com>
2025-05-12 12:50:55 -0600

    Deleted column properties of the table.
    Commented several properties in CRUD files, model file.
    Added new migration and updated DB.

---
+++ RazorMovies/Migrations/20250512184436_RemoveSeveralPropertiesFromMovie.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSeveralPropertiesFromMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cast",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "TrailerUrl",
+                table: "Movie");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cast",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Duration",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrailerUrl",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- RazorMovies/Models/Movie.cs
+++ RazorMovies/Models/Movie.cs
@@ -6,17 +6,16 @@ namespace RazorMovies.Models
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Genre { get; set; }
-        // public string? Director { get; set; }
-        public string? Description { get; set; }
-        public string? ImageUrl { get; set; }
-        public string? TrailerUrl { get; set; }
         public string? Rating { get; set; }
-        public string? Duration { get; set; }
-        public string? Language { get; set; }
-        public string? Country { get; set; }
-        public string? Cast { get; set; }
-
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        // public string? Duration { get; set; }
+        // public string? Language { get; set; }
+        // public string? Country { get; set; }
+        // public string? Cast { get; set; }
+        // public string? Director { get; set; }
+        // public string? Description { get; set; }
+        // public string? ImageUrl { get; set; }
+        // public string? TrailerUrl { get; set; }
     }
 }
