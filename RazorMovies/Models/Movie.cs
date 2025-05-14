35803703f0bb3971685bec0f0be2880987424f5b
Alex <acst352@gmail.com>
2025-05-14 16:05:06 -0600

    Changes

--- RazorMovies/Models/Movie.cs
+++ RazorMovies/Models/Movie.cs
@@ -6,10 +6,11 @@ namespace RazorMovies.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        public string? Genre { get; set; }
-        public string? Rating { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        public string? Rating { get; set; }
+        public string? Genre { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         // public string? Duration { get; set; }
