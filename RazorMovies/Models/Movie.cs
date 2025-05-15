f098da9ec4ccfe09da90651474c2eefda3e4969c
Alex <acst352@gmail.com>
2025-05-15 13:57:51 -0600

    Rating field Empty

--- RazorMovies/Models/Movie.cs
+++ RazorMovies/Models/Movie.cs
@@ -9,7 +9,7 @@ namespace RazorMovies.Models
         [DataType(DataType.Date)]
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
-        public string? Rating { get; set; }
+        public string Rating { get; set; } = string.Empty;
         public string? Genre { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
