2dc9b0580cde8e7d74eba432f93c3c535ece4465
Alex <acst352@gmail.com>
2025-05-15 15:51:10 -0600

    Adding Model Validations

--- RazorMovies/Models/Movie.cs
+++ RazorMovies/Models/Movie.cs
@@ -5,14 +5,23 @@ namespace RazorMovies.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string? Title { get; set; } = string.Empty;
         [DataType(DataType.Date)]
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
-        public string Rating { get; set; } = string.Empty;
-        public string? Genre { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; } = string.Empty;
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; } = string.Empty;
         // public string? Duration { get; set; }
         // public string? Language { get; set; }
         // public string? Country { get; set; }
